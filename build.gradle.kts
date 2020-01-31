24011e46da8b06e115a877e8079f4b4a9df99766
Jens Stegemann <jens.stegemann@oeffentliche.de>
2020-01-31 23:07:06 +0100

    first

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+
+buildscript {
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+    }
+}
+
+allprojects {
+    //TODO: manage common setting and dependencies
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+        jcenter()
+    }
+}
+
+subprojects {
+    group = "io.fritz2.optics"
+    version = "0.1"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,6 @@
+rootProject.name = "fritz2-optics"
+
+include("compiler")
+include("plugin")
+include("shared")
+include("core")
